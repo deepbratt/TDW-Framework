c6cbb563049743fd0f5798655f20485d40ea841f
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-07-06 15:17:30 +0500

    setup redux store

---
+++ src/redux/reducers/authSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface IInitialState {
+  user?: object;
+  isLoggedIn: boolean;
+  token?: string;
+}
+
+const initialState: IInitialState = {
+  user: {},
+  isLoggedIn: true,
+  token: "",
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isLoggedIn = true;
+      state.user = action.payload.data.user;
+      state.token = action.payload.data.token;
+      console.log("payload", action.payload.data.user);
+    },
+    logout: (state) => {
+      state.user = {};
+      state.token = "";
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;

166b4d40e6649468c3eed3b25590574211a03e50
jamshedahmed19 <jamsheda4ahmed786@gmail.com>
2021-07-09 22:59:55 +0500

    deleted package-lock.json

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,9 +1,9 @@
 import { Grid, AppBar, Toolbar, Typography } from "@material-ui/core";
 // import {Home}  from "tdw-pages";
 // import HeaderContext from "../Sections/Header/Header";
-import {CommonButton}  from "tdw-components";
+// import {CommonButton}  from "tdw-components";
 // import { Footer } from "tdw-components";
-//import Footer from "./footer";
+import Footer from "./footer";
 // import { FooterData } from "../utils/constants/language/en/footerData";
 
 export interface LayoutProps {
@@ -20,14 +20,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div>
       <Grid container>
         <Grid item xs={12}>
-          <CommonButton />
+          {/* <CommonButton /> */}
         </Grid>
         <Grid item xs={12}>
           {children}
         </Grid>
         <Grid item xs={12}>
           {/* // * Footer here */}
-          {/* <Footer data={FooterData} /> */}
+          <Footer data={FooterData} />
         </Grid>
       </Grid>
     </div>

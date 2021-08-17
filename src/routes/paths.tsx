b79a8e22e10cae6a891bf2c066dcd0faad3c9c0a
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-08-17 17:44:36 +0500

    removed unused var

--- src/routes/paths.tsx
+++ src/routes/paths.tsx
@@ -15,7 +15,7 @@ const CarDetail = lazy(() => import("../Pages/carDetail"));
 const CarComparison = lazy(() => import("../Pages/carComparision/index"));
 const ShortlistItem = lazy(() => import("../Pages/shortlistItems"));
 // const Dashboard = lazy(() => import("../Pages/dashboard/index")) ;
-const CarListing = lazy(() => import("../Pages/postAd"));
+// const CarListing = lazy(() => import("../Pages/postAd"));
 
 export const paths = {
   home: "home",

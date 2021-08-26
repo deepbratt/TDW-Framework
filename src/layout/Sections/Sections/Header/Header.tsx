be939fe7d9ade97ec3acc81412bfab01e718d7cc
Abdul Zahir <abdul.zahir@digitalpumpkins.com>
2021-08-26 23:29:27 +0500

    background color to app bar - header

--- src/layout/Sections/Sections/Header/Header.tsx
+++ src/layout/Sections/Sections/Header/Header.tsx
@@ -75,7 +75,7 @@ const HeaderContext = () => {
 
   return (
     <React.Fragment>
-      <AppBar className={root} color="transparent" style={{background:"white"}}>
+      <AppBar className={root} style={{background:"white", color:"white"}}>
         <Hidden mdDown>
           <img src={header} alt="logo" className={rec} />
         </Hidden>
--- src/layout/Sections/Sections/Header/useStyles.tsx
+++ src/layout/Sections/Sections/Header/useStyles.tsx
@@ -10,6 +10,7 @@ export const useStyles = makeStyles((theme) => ({
     padding: '0 50px',
     backgroundColor: white,
     background:"white",
+    color:"white",
     boxShadow: '0 1px 2px rgba(0,0,0,0.07)',
     [breakpoints.down('sm')]: {
       padding: '0px'

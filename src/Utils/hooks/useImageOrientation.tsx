9079493b0defc2de04e128a577a049c39617721d
basuabhirup <basu.abhirup@yahoo.com>
2021-11-30 23:44:13 +0530

    Some minor refactoring

--- src/Utils/hooks/useImageOrientation.tsx
+++ src/Utils/hooks/useImageOrientation.tsx
@@ -3,34 +3,34 @@ import { useState } from 'react';
 const useImageOrientation = () => {
   const [imgHeight, setImgHeight] = useState('auto');
   const [imgWidth, setImgWidth] = useState('auto');
-  const [imageOrientation, setImageOrientation] = useState('');
+  // const [imageOrientation, setImageOrientation] = useState('');
 
-  const getImageOrientation = (imgUrl: string) => {
-    let img = new Image();
-    img.src = imgUrl;
-    let orientationImg = '';
-    // img.onload = function () {
-    if (img.naturalWidth > img.naturalHeight) {
-      orientationImg = 'landscape';
-    } else if (img.naturalWidth < img.naturalHeight) {
-      orientationImg = 'portrait';
-    } else {
-      orientationImg = 'even';
-    }
-    // }
-    return orientationImg;
-  };
+  // const getImageOrientation = (imgUrl: string) => {
+  //   let img = new Image();
+  //   img.src = imgUrl;
+  //   let orientationImg = '';
+  //   // img.onload = function () {
+  //   if (img.naturalWidth > img.naturalHeight) {
+  //     orientationImg = 'landscape';
+  //   } else if (img.naturalWidth < img.naturalHeight) {
+  //     orientationImg = 'portrait';
+  //   } else {
+  //     orientationImg = 'even';
+  //   }
+  //   // }
+  //   return orientationImg;
+  // };
 
   const setImageOrientationAndSize = (imgUrl: string) => {
-    let imgOrientation = getImageOrientation(imgUrl);
-    setImageOrientation(imgOrientation);
-    let height=""
-    let width=""
+    // let imgOrientation = getImageOrientation(imgUrl);
+    // setImageOrientation(imgOrientation);
+    // let height=""
+    // let width=""
     // if (imgOrientation === 'portrait') {
-      setImgHeight('100%');
-      setImgWidth('auto');
-      height="100%"
-      width="auto"
+    setImgHeight('100%');
+    setImgWidth('auto');
+    let height = "100%";
+    let width = "auto";
     // } else {
     //   setImgWidth('100%');
     //   setImgHeight('auto');
@@ -45,8 +45,8 @@ const useImageOrientation = () => {
     imgWidth,
     setImgHeight,
     setImgWidth,
-    setImageOrientation,
-    imageOrientation
+    // setImageOrientation,
+    // imageOrientation
   };
 };
 

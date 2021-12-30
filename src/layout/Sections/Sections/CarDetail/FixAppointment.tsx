002858c1b92deb8981b6a2f692a525db8a13b54f
basuabhirup <basu.abhirup@yahoo.com>
2021-12-30 22:50:33 +0530

    Update FixAppointment Component

--- src/layout/Sections/Sections/CarDetail/FixAppointment.tsx
+++ src/layout/Sections/Sections/CarDetail/FixAppointment.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../../redux/store';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { useStyles } from './useStyles';
-import { Phone } from '@material-ui/icons';
+import PhoneCallbackIcon from '@material-ui/icons/PhoneCallback';
+import AppointmentForm from '../../../../components/AppointmentForm';
 
-const FixAppointment: React.FC<any> = () => {
+const FixAppointment: React.FC<any> = ({ }) => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const [appointmentForm, setAppointmentForm] = useState(false);
   const { mailBtn } = useStyles();
   return (
-    <Grid container style={{ display: 'inline-block', padding: "20px" }}>
-      <Typography variant="h2">Get your Car Verified with a Carokta certified mechanic</Typography>
-      <Button
-        // fullWidth
-        className={mailBtn}
-        startIcon={<Phone />}
-        // onClick={handleShowPhone}
+    <Grid container style={{
+      display: 'flex',
+      flexDirection: "column",
+      margin: "20px auto",
+      padding: "20px",
+      alignItems: "center",
+      justifyContent: "center"
+    }}>
+      <Typography variant="h2">Get your Car Verified with a Carokta certified mechanic:</Typography>
+      <Button 
+        className={mailBtn} 
+        startIcon={<PhoneCallbackIcon />}
+        style={{ margin: "20px" }} 
+        onClick={() => setAppointmentForm(true)}
       >
         Schedule an Appointment
       </Button>
+      <AppointmentForm
+        open={appointmentForm}
+        handleClose={() => setAppointmentForm(false)}
+        fullName={user.firstName ? `${user.firstName} ${user.lastName}` : ''}
+        phone={user.phone}
+        handleSubmit={(name, number) => null}
+      />
     </Grid>
   )
 }
